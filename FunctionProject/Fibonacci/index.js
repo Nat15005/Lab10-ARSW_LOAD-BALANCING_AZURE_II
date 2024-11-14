32d53ca9c1ecc3f0b1b8dab8ef843b14ee5eea9a
unknown <ana.duran-b@labinfo.is.escuelaing.edu.co>
2024-11-14 09:58:36 -0500

    En clase
    Co-authored-by: Nat <laura.rojas-r@mail.escuelaing.edu.co>

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,12 +1,12 @@
 var bigInt = require("big-integer");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
+ 
     let nth = req.body.nth
     let nth_1 = bigInt.one;
     let nth_2 = bigInt.zero;
     let answer = bigInt.zero;
-
+ 
     if (nth < 0)
         throw 'must be greater than 0'
     else if (nth === 0)
@@ -20,7 +20,7 @@ module.exports = async function (context, req) {
             nth_1 = answer
         }
     }
-
+ 
     context.res = {
         body: answer.toString()
     };
